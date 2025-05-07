41a53d5f407e034000ca5c4ef73e5f6a4efa14c9
RenatoLXIX <renato.duran2101@alumnos.ubiobio.cl>
2025-05-07 19:00:04 -0400

    Carpetas bases para cada microserrvicio con su backend y frontend

---
+++ Modulo media/backendMedia/src/test/java/com/proyectogps/backendMedia/BackendMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectogps.backendMedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Modulo sostenedor/backendSostenedor/src/main/java/com/proyectogps/backendSostenedor/BackendSostenedorApplication.java
@@ -0,0 +1,13 @@
+package com.proyectogps.backendSostenedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSostenedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSostenedorApplication.class, args);
+	}
+
+}
