47d37635297e5f1f20260ad129a8d33e9b632bb3
paula-ss1 <paula.mellado2101@alumnos.ubiobio.cl>
2025-07-20 23:16:55 -0400

    front basico

--- Modulo Basica/backendBasica/src/main/java/com/proyectogps/backendBasica/Controller/MaterialComplementarioController.java
+++ Modulo Basica/backendBasica/src/main/java/com/proyectogps/backendBasica/Controller/MaterialComplementarioController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+
 @RequestMapping("/api/materiales")
 @CrossOrigin(origins = "*") // Permite llamadas desde el frontend
 public class MaterialComplementarioController {
--- Modulo Basica/frontEndBasica/src/app/app.component.ts
+++ Modulo Basica/frontEndBasica/src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontEndBasica';
+export class AppComponent implements OnInit {
+  planificaciones: any[] = [];
+  recursos: any[] = [];
+  materiales: any[] = [];
+  isLoading = true;
+  errorMessage = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData(): void {
+    // Cargar planificaciones
+    this.http.get<any[]>('http://localhost:8080/api/planificaciones').subscribe({
+      next: (data) => {
+        console.log('Planificaciones:', data);
+        this.planificaciones = data;
+      },
+      error: (err) => {
+        console.error('Error planificaciones:', err);
+        this.errorMessage = 'Error cargando planificaciones';
+      }
+    });
+
+    // Cargar recursos
+    this.http.get<any[]>('http://localhost:8080/api/recursos').subscribe({
+      next: (data) => {
+        console.log('Recursos:', data);
+        this.recursos = data;
+      },
+      error: (err) => {
+        console.error('Error recursos:', err);
+        this.errorMessage = 'Error cargando recursos';
+      }
+    });
+
+    // Cargar materiales
+    this.http.get<any[]>('http://localhost:8080/api/materiales').subscribe({
+      next: (data) => {
+        console.log('Materiales:', data);
+        this.materiales = data;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error('Error materiales:', err);
+        this.errorMessage = 'Error cargando materiales';
+        this.isLoading = false;
+      }
+    });
+  }
 }
